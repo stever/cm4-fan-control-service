6fcb56df4a83a618c2a244c36e8be4489ef87d8f
Steven Robertson <steverobertson@gmail.com>
2021-07-22 10:10:39 +0000

    Set fan speed on service startup

--- fancontrol.py
+++ fancontrol.py
@@ -1,10 +1,17 @@
+import os
 import time
+
 from datetime import datetime
 
-path_to_file = '/home/ubuntu/fancontrol.log'
+home = '/home/ubuntu/'
+proj = f'{home}git/cm4-fan-control-service/'
+main = f'{proj}main'
+csv = f'{home}fancontrol.csv'
+
+os.system(f'{main} set 128')
 
 while True:
-    with open(path_to_file, 'a') as f:
+    with open(csv, 'a') as f:
         f.write('The current timestamp is: ' + str(datetime.now()) + '\n')
         f.close()
 

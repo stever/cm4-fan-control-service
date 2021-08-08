b681e2245a7fae0676ccef1f0c80a586903209f8
Steven Robertson <steverobertson@gmail.com>
2021-08-08 18:09:36 +0000

    Adjusting fan starting temp

--- fancontrol.py
+++ fancontrol.py
@@ -26,8 +26,8 @@ load_dotenv(path_dotenv)
 elastic_host = os.getenv('ELASTICSEARCH_HOST')
 elastic_pass = os.getenv('ELASTICSEARCH_PASSWORD')
 elastic_user = 'elastic'
-fan_min_temp = 40.0 
-fan_full_temp = 50.0
+fan_min_temp = 48.0 
+fan_full_temp = 58.0
 sleep_secs = 1
 
 # Machine info
